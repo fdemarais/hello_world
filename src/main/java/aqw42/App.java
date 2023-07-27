f76f1e5afb88e96bd8995b300afe9bc9a199e40c
DEMARAIS Frederic <frederic.demarais@infotel.com>
2023-07-27 15:57:02 +0200

    fix errors

--- src/main/java/aqw42/App.java
+++ src/main/java/aqw42/App.java
@@ -8,11 +8,11 @@ public class App
 {
     public static String get_message()
     {
-        return "Hello World !"
+        return "Hello World !";
     }
 
     public static void main( String[] args )
     {
-        System.out.println( get_message() )
+        System.out.println( get_message() );
     }
 }
